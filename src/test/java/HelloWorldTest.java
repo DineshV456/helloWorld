3b70e01a2a15bead99465488d56d70ea2899b111
DineshV456 <dineshkumar.venkatesh@emergertech.com>
2025-09-15 15:37:38 +0530

    files pushed

---
+++ src/test/java/HelloWorldTest.java
@@ -0,0 +1,9 @@
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class HelloWorldTest {
+    @Test
+    public void testHello() {
+        assertTrue(true); // simple test just to check CI
+    }
+}
