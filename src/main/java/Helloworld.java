168acf85c65a832dedb5c210b74adc61450d2177
DineshV456 <dineshkumar.venkatesh@emergertech.com>
2025-09-15 17:20:16 +0530

    changes edited

--- src/main/java/Helloworld.java
+++ src/main/java/Helloworld.java
@@ -2,7 +2,7 @@ public class Helloworld {
     public static void main(String[] args) {
         System.out.println("Hello, CI!");
         System.out.println("Hello, CI with PR!");
-        int x = "string";
+        
 
     }
 }
